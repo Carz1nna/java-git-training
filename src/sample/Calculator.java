328ecb73aa9556adf31ba5baf7c249f8c6f6c7c9
Igor <123@gmail.com>
2026-01-15 14:04:10 +0300

    Добавил метод power() для возведения числа в степень

--- src/sample/Calculator.java
+++ src/sample/Calculator.java
@@ -1,14 +1,46 @@
 package sample;
 import java.util.Scanner;
 public class Calculator {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner scanner = new scanner(System.in);
         int fst = scanner.nextInt();
         int scnd = scanner.nextInt();
+
+        int dei = scanner.nextInt(); // 1+2-3*4/5^
+        switch (dei) {
+            case '1':
+                int plus = fst+scnd;
+            case '2':
+                int minus = fst-scnd;
+            case '3':
+                int umn = fst*scnd;
+            case '4':
+                int del = fst/scnd;
+        }
 
         int plus = fst+scnd;
         int minus = fst-scnd;
         int umn = fst*scnd;
         int del = fst/scnd;
+        return fst;
+
+
     }
+
+    public static <scnd, fst> int power (fst, scnd) {
+        int pow = fst^scnd;
+    }
+    switch (dei) {
+        case '1':
+            System.out.println(plus);
+        case '2':
+            System.out.println(minus);
+        case '3':
+            System.out.println(umn);
+        case '4':
+            System.out.println(del);
+        case '5':
+            System.out.println(pow);
+    }
+    System.out.println()
 }
